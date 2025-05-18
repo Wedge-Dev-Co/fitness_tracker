60afb46ba6f1ec6a9c121424782b9b571ae38782
Taylor C. Powell <taylorp661@gmail.com>
2025-05-18 16:00:03 -0400

    Added tkinter stuff

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-# This is a simple nutrition tracking program
+# This is a simple fitness tracking program
 # It allows the user to log exercise and nutrition data on a daily basis
 # It also allows the user to view their progress over time
 
+import tkinter as tk
+from tkinter import *
 
+def new_entry():
+    print("New Entry")
 
 def start_app():
     print("Welcome to the Hi-Fi Fitness Tracker!")
 	# TODO: Implement the main application logic here
-	
+    root = Tk()
+    root.title("Hi-Fi Fitness Tracker")
+    root.geometry("400x400")
+    btn = Button(root, text = "Create new entry", command = new_entry)
+    btn.grid(column=2, row=2)
+    root.mainloop()
 
 start_app()
